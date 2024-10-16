9a861003b6368ade0b8abae9731b8b8a4f00521f
Makc Kushnir <Makc Kushnir@KUSHNIR>
2024-10-16 16:27:40 +0300

    Добавьте файлы проекта.

---
+++ lr7/Controllers/FileController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace lr7.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DownloadFile(string firstName, string lastName, string fileName)
+        {
+            var fileContent = $"Ім'я: {firstName}\nПрізвище: {lastName}";
+            var byteArray = Encoding.UTF8.GetBytes(fileContent);
+            var stream = new System.IO.MemoryStream(byteArray);
+
+            return File(stream, "text/plain", $"{fileName}.txt");
+        }
+    }
+}
